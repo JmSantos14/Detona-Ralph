21fe835dc1dba79d8f4f68de8bfbee115de0b8ec
santos <jmsdo1970@gmail.com>
2025-09-17 13:29:18 -0300

    fim projeto web

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -3,19 +3,23 @@ const state = {
         squares: document.querySelectorAll(".square"),
         enemy: document.querySelector(".enemy"),
         timeLeft: document.getElementById("time-left"),
-        score: document.getElementById("score")
+        score: document.getElementById("score"),
+        lifeCount: document.getElementById("lifeCount"),
+        easyMode: document.getElementById("easyMode"),
+        hardMode: document.getElementById("hardMode")
     },
     values:{
         timerId: null,
-        gameVelocity: 700,
+        gameVelocity: 650,
         hitPosition: 0,
         result: 0,
         currentTime: 45,
-        countDownTimerId: setInterval(countDown, 1000)
+        countDownTimerId: setInterval(countDown, 1000),
+        lifeId: 3
     }
 }
 
-function countDown(){
+async function countDown(){
     state.values.currentTime--;
     state.view.timeLeft.textContent = state.values.currentTime
 
@@ -24,6 +28,12 @@ function countDown(){
         clearInterval(state.values.timerId)
         alert("Game Over! O seu Resultado foi: " + state.values.result)
     }
+}
+
+function playSound(){
+    let audio = new Audio("./src/audio/hit.m4a")
+    audio.volume = .1
+    audio.play()
 }
 
 function randomSquare(){
@@ -37,11 +47,20 @@ function randomSquare(){
     state.values.hitPosition = randomSquare.id
 }
 
-function moveEnemy(){
+async function moveEnemy(){
     state.values.timerId = setInterval(randomSquare, state.values.gameVelocity)
 }
 
-
+function loseLife(){
+    state.values.lifeId--
+    state.view.lifeCount.textContent = state.values.lifeId
+    state.values.hitPosition = null
+    if(state.values.lifeId === 0){
+        clearInterval(state.values.countDownTimerId)
+        clearInterval(state.values.timerId)
+        alert("Game Over! O seu Resultado foi: " + state.values.result)
+    }
+}
 
 function addListenerHitBox(){
     state.view.squares.forEach((square) => {
@@ -50,16 +69,38 @@ function addListenerHitBox(){
                 state.values.result++
                 state.view.score.textContent = state.values.result
                 state.values.hitPosition = null
+                playSound()
+            }else{
+                loseLife()
             }
         })
     })
 }
 
+function resetGame(){
+    location.reload();
+}
 
-function init(){
+function changeToEasy(){
+    localStorage.setItem("difficulty", "easy")
+    resetGame()                     
+}
+
+function changeToHard(){
+    localStorage.setItem("difficulty", "hard")
+    resetGame()
+}
+
+
+function init(){ const difficulty = localStorage.getItem("difficulty")
+    if (difficulty === "hard") {
+        state.values.gameVelocity = 200
+    } else {
+        state.values.gameVelocity = 650
+    }
+    
     moveEnemy()
     addListenerHitBox()
 }
-
 
 init()
